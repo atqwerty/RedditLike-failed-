ee8c9e87b9db477105a2079da3cf3254bb5b02c5
Маркитанов Денис <dmarkitanov@gmail.com>
2018-06-20 23:04:00 +0600

    final fail

---
+++ RedditLike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedditLike.Startup))]
+namespace RedditLike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
